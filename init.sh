d7037ae797a173485d53db0b5dc2c9bc5ed44f47
Tridiktya Putra <tridiktya@gmail.com>
2023-10-19 09:49:37 +0700

    first commit

---
+++ init.sh
@@ -0,0 +1,6 @@
+git init
+git add --all -- ':init.sh'
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/trdkhardani/laravel-simple-api.git
+git push -u origin main
